bb1f74c1f0ede3be8add9ccc155d0ef46dba2a1d
zhangbin <zhangbin_1012@icloud.com>
2019-05-14 13:49:26 +0800

    代理

--- config.go
+++ config.go
@@ -4,6 +4,8 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 type Config struct {
@@ -13,10 +15,10 @@ type Config struct {
 
 func parseConfig() Config {
 	cfg := Config{}
-	//dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	//if err != nil {
-	//	log.Fatalf("filepath not found err #%v ", err)
-	//}
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatalf("filepath not found err #%v ", err)
+	}
 	yamlFile, err := ioutil.ReadFile(dir + "/conf/config.yaml")
 	if err != nil {
 		log.Printf("conf/config.yaml not found err #%v ", err)
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module reverseProxy-go
 
-require gopkg.in/yaml.v2 v2.2.2
+require (
+	github.com/bogdanovich/dns_resolver v0.0.0-20170211073258-a8e42bc6a5b6
+	github.com/miekg/dns v1.1.9 // indirect
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
+	golang.org/x/net v0.0.0-20190509222800-a4d6f7feada5 // indirect
+	golang.org/x/sys v0.0.0-20190509141414-a5b02f93d862 // indirect
+	gopkg.in/yaml.v2 v2.2.2
+)
