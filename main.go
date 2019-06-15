d19692ab0e02cf700f5da9cdf8a64bc91d0a0e68
zhangbin <zhangbin_1012@icloud.com>
2019-06-15 17:10:03 +0800

    反向代理

--- main.go
+++ main.go
@@ -1,75 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
-	"net"
 	"net/http"
+	"net/http/httputil"
 	"strings"
 )
 
 var cfg Config
 
 func main() {
-	cfg = parseConfig()
-
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	director := func(req *http.Request) {
+		var domain string
+		i := strings.IndexByte(req.Host, ':')
+		if i < 0 {
+			domain = req.Host
+		} else {
+			domain = req.Host[0:i]
+		}
+		port, ok := cfg.Route[domain];
+		if !ok {
+			log.Println(domain + " not found")
+			return
+		}
+		req.URL.Host = fmt.Sprintf("%s:%s", domain, port)
+	}
+	proxy := &httputil.ReverseProxy{Director: director}
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), proxy)
 	if err != nil {
 		log.Panic(err)
-	}
-
-	for {
-		client, err := l.Accept()
-		if err != nil {
-			log.Panic(err)
-		}
-
-		go handleClientRequest(client)
-	}
-}
-
-func handleClientRequest(client net.Conn) {
-	if client == nil {
 		return
 	}
-	defer client.Close()
-
-	var b [1024]byte
-	n, err := client.Read(b[:])
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	reader := bufio.NewReader(strings.NewReader(string(b[:])))
-	req, err := http.ReadRequest(reader)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	var domain string
-	i := strings.IndexByte(req.Host, ':')
-	if i < 0 {
-		domain = req.Host
-	} else {
-		domain = req.Host[0:i]
-	}
-	port, ok := cfg.Route[domain];
-	if !ok {
-		log.Println(domain + " not found")
-		return
-	}
-	//获得了请求的host和port，就开始拨号吧
-	server, err := net.Dial("tcp", domain+":"+port)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	server.Write(b[:n])
-	//进行转发
-	go io.Copy(server, client)
-	io.Copy(client, server)
 }
